21bd7b2a78cf11bef351547b0d40fbdb139cd055
Kim Seongeun <cellnews0514@gmail.com>
2024-10-30 15:39:59 +0900

    [feat] (김성은) 프론트 폴더 구조

--- front/babel.config.js
+++ front/babel.config.js
@@ -2,5 +2,30 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./app"],
+          extensions: [
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".tsx",
+            ".jsx",
+            ".js",
+            ".json",
+          ],
+          alias: {
+            '@': './app',
+            '@components': './app/components',
+            '@screens': './app/screens',
+            '@assets': './app/assets',
+          },
+        },
+      ],
+    ],
   };
 };
