6066e2161ef62f501e10647dbb9e1e1e139cde5f
Kim Seongeun <cellnews0514@gmail.com>
2024-10-31 09:08:49 +0900

    [feat] (김성은) 버튼 컴포넌트 생성

---
+++ front/app/components/Button/SquareBtn/SquareBtn.js
@@ -0,0 +1,49 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
+
+export default function SquareBtn({text, onPress}) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity 
+        style={styles.touchableOpacity}
+        onPress={onPress}
+        >
+        <LinearGradient 
+          style={styles.gradientButton}
+          start={{ x: 0, y: 0.5}} // 왼쪽 중앙 시작
+          end={{x: 1, y: 0.5}} // 오른쪽 중앙 종료 
+          colors={['#FF740E', '#FFA646']}
+        >
+            <Text style={styles.btnText}>{text}</Text>
+        </LinearGradient>
+      </TouchableOpacity>
+      {/* <StatusBar style="auto" /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // flexDirection: 'row',
+    // backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  touchableOpacity: { 
+    flex: 1, 
+    flexDirection: 'row'
+  },
+  gradientButton: {
+    flex: 0.75,
+    // height: 60,
+    aspectRatio: 5, // 가로가 세로의 5배 비율
+    backgroundColor: '#FF740E',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12,
+  },
+  btnText: {    
+    color: 'white',
+  }
+});
---
+++ front/app/components/InputBox/InputBox.js
@@ -0,0 +1,45 @@
+import { View, StyleSheet, TextInput, Text } from 'react-native';
+
+const InputBox = ({label, placeholder}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Text style={styles.label}>{label}</Text>
+        <TextInput
+          style={styles.input}
+          placeholder={placeholder}
+        />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    // backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  innerContainer: {
+    flex: 0.75,
+  },
+  label: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    fontSize: 15,
+  },
+  input: {
+    // flex: 0.7,
+    aspectRatio: 5, // 가로가 세로의 4.5배 비율
+    borderRadius: 12,
+    borderColor: '#D5D5D5',
+    borderWidth: 1,
+    paddingHorizontal: 18,
+    backgroundColor: 'white',
+    fontSize: 15,
+  }
+})
+
+export default InputBox
