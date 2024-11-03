bd7fd5d129780cdfe7a85832acc86f6a68ac4d8e
Kim Seongeun <cellnews0514@gmail.com>
2024-11-04 08:50:30 +0900

    [feat] (김성은) 알림창 생성중

--- front/app/screens/Notification/NotificationScreen.js
+++ front/app/screens/Notification/NotificationScreen.js
@@ -2,13 +2,15 @@ import {
   Wapper,
 } from "./NotificationScreenStyles"
 import { Text } from "react-native";
+import TeamRoundBtn from "../../components/Button/TeamRoundBtn/TeamRoundBtn";
 import colors from "../../styles/colors";
 
 
 const NotificationScreen = () => {
   return(
     <Wapper>
-      <Text>알림창</Text>
+      <Text>팀신청</Text>
+      <TeamRoundBtn text={'수락'} onPress={() => console.log("수락 버튼 클릭")} backColor={colors.o_btn} />
     </Wapper>
   )
 }
