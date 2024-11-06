b9a9f090b7f18346e993a366a6babd309772d709
dpqls0356 <dpqls0356@gmail.com>
2024-11-07 07:11:33 +0900

    [style](이예빈)루트 상세페이지 퍼블리싱

---
+++ front/screens/RouteDetail/RouteDetailStyle.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { SafeAreaView, View, Text, TouchableOpacity } from "react-native";
+import colors from "../../styles/colors";
+import fonts from "../../styles/fonts";
+export const Wrapper = styled.View``;
+
+export const Top = styled.View``;
+
+export const Bottom = styled.View``;
