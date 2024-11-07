46b13475be9db5c67119d041efe849362ee5bcfc
leedhfsd <leedhfsd@gmail.com>
2024-11-08 08:49:31 +0900

    [feat](이대현) aws s3추가

--- back/route/src/main/java/com/easter/route/domain/AwsS3/AwsS3Controller.java
+++ back/route/src/main/java/com/easter/route/domain/AwsS3/AwsS3Controller.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/maon")
 public class AwsS3Controller {
 
 	private final AmazonS3 amazonS3Client;
