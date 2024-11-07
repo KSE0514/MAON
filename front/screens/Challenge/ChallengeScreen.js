000a858c373ee570cdb736ccdf5785fce349774a
Kim Seongeun <cellnews0514@gmail.com>
2024-11-07 16:36:55 +0900

    [feat] (김성은) 챌린지 화면 퍼블리싱 중

--- front/screens/Challenge/ChallengeScreen.js
+++ front/screens/Challenge/ChallengeScreen.js
@@ -1,10 +1,25 @@
 import {
-  Wrapper
+  Wrapper,
+  BackColor,
+  WhitePathView,
+  MountainView,
+  FooterArea,
+  ChallengeArea,
+  ChallengeLevelText,
+  ChallengeNextLevelText,
+  ChallengeText,
+  FooterIconArea,
 } from "./ChallengeScreenStyles"
 
-import { SafeAreaView, View, Text, Button, TouchableOpacity, ScrollView } from "react-native";
+import { SafeAreaView, View, Text, Button, TouchableOpacity, ScrollView, Image } from "react-native";
 import { useFontsLoaded } from "../../utils/fontContext";
 import Svg, { Path, Defs, LinearGradient, Stop } from 'react-native-svg';
+import Mountain from "../../components/ChallengeSvg/Mountain/Mountain";
+import WhitePath from "../../components/ChallengeSvg/WhitePath/WhitePath";
+import WhitePoint from "../../components/ChallengeSvg/WhitePoint/WhitePoint";
+import ghost from "./../../assets/images/challenge_ghost.png"
+import trophy from "./../../assets/images/challenge_Trophy.png"
+
 const testMode = true
 
 
@@ -15,163 +30,120 @@ const ChallengeScreen = ({ navigation }) => {
     return null; // 폰트 로드 전까지 렌더링 방지
   }
   return (
-    <Wrapper>
-      {testMode?
-      <ScrollView>
-        <Svg width="321" height="240" viewBox="0 0 321 240" fill="none">
-        <Path
-          d="M200.878 5.36357L0.5 111.5V239.5H430V111.5L237.956 5.67049C226.439 -0.676224 212.499 -0.791624 200.878 5.36357Z"
-          fill="url(#paint0_linear_2006_177)"
-        />
-        <Defs>
-          <LinearGradient
-            id="paint0_linear_2006_177"
-            x1="105"
-            y1="78"
-            x2="289.5"
-            y2="142"
-            gradientUnits="userSpaceOnUse"
+    <BackColor>
+
+      <Wrapper>
+
+        {testMode?
+          <View
+            style={{zIndex: 10000}}
           >
-            <Stop stopColor="#FDD048" />
-            <Stop offset="1" stopColor="#FF740E" />
-          </LinearGradient>
-        </Defs>
-      </Svg>
+            <Text>챌린지 화면</Text>
+              <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate("Login");
+                  }}
+                >
+                  <Text style={{fontSize: 30}}>로그인 화면</Text>
+                </TouchableOpacity>
+              <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate("MyPage");
+                  }}
+                >
+                  <Text style={{fontSize: 30}}>마이페이지</Text>
+                </TouchableOpacity>
+              <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate("CreateTeam");
+                  }}
+                >
+                  <Text style={{fontSize: 30}}>팀 생성</Text>
+                </TouchableOpacity>
+              <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate("MarathonEntryForm");
+                  }}
+                >
+                  <Text style={{fontSize: 30}}>마라톤 신청서</Text>
+                </TouchableOpacity>
+              <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate("MarathonInfoDetail");
+                  }}
+                >
+                  <Text style={{fontSize: 30}}>마라톤 정보 디테일</Text>
+                </TouchableOpacity>
+
+          </View>
+         :
+          null}
+          <FooterArea>
+            <ChallengeArea>
+              <ChallengeLevelText>
+                Lv. 3
+              </ChallengeLevelText>
+              <ChallengeNextLevelText>
+                달성 조건:
+              </ChallengeNextLevelText>
+              <ChallengeText>
+                  5km 이상 1회 달리기
+              </ChallengeText>
+            </ChallengeArea>
+            <FooterIconArea>
+              <Image
+                style={{ width: "100%", height: "100%", resizeMode: "contain", transform: [{ scaleX: -1 }]  }}
+                source={trophy}
+              />
+              {/* <Image
+                // style={{width: '50px', height: '50px', position: 'relative', zIndex: 1000000}}
+                source={trophy} /> */}
+            </FooterIconArea>
+          </FooterArea>
 
 
-      <Svg width="234" height="415" viewBox="0 0 234 415" fill="none">
-        <Path
-          d="M-120 0.5L233.5 197.5V415H-120V0.5Z"
-          fill="url(#paint0_linear_2006_176)"
-        />
-        <Defs>
-          <LinearGradient
-            id="paint0_linear_2006_176"
-            x1="151.5"
-            y1="245.5"
-            x2="64"
-            y2="142.5"
-            gradientUnits="userSpaceOnUse"
+          {/* 유령 */}
+          <WhitePathView
+          style={{zIndex: 5, right: 0, bottom: 200}}
           >
-            <Stop stopColor="#FA9987" />
-            <Stop offset="1" stopColor="#FA7514" />
-          </LinearGradient>
-        </Defs>
-      </Svg>
+            <Image source={ghost} />
+          </WhitePathView>
 
-
-
-
-      <Svg width="391" height="402" viewBox="0 0 391 402" fill="none">
-        <Path
-          d="M-190 402L391 0V402H-190Z"
-          fill="url(#paint0_linear_2006_175)"
-        />
-        <Defs>
-          <LinearGradient
-            id="paint0_linear_2006_175"
-            x1="263"
-            y1="90"
-            x2="375"
-            y2="259.5"
-            gradientUnits="userSpaceOnUse"
+          {/* 미션 원 */}
+          <WhitePathView>
+            <WhitePoint />
+          </WhitePathView>
+          {/* 미션 길 */}
+          <WhitePathView>
+            <WhitePath />
+          </WhitePathView>
+          <MountainView
+            style={{zIndex: 3, right: 0}}
           >
-            <Stop stopColor="#FA9987" />
-            <Stop offset="0.128584" stopColor="#FA9987" />
-            <Stop offset="1" stopColor="#A76251" />
-          </LinearGradient>
-        </Defs>
-      </Svg>
-
-
-
-      <Svg width="391" height="575" viewBox="0 0 391 575" fill="none">
-        <Path
-          d="M-1 574.5V0L102.425 73.0873C107.939 76.9832 115.234 77.2479 121.015 73.7617L161.422 49.3945C170.838 43.7158 182.758 44.3152 191.557 50.9098L400.5 207.5V574.5H-1Z"
-          fill="url(#paint0_linear_2006_174)"
-        />
-        <Defs>
-          <LinearGradient
-            id="paint0_linear_2006_174"
-            x1="119"
-            y1="204.5"
-            x2="107"
-            y2="88"
-            gradientUnits="userSpaceOnUse"
+            <Mountain number={1}/>
+          </MountainView>
+          <MountainView
+            style={{zIndex: 2}}
           >
-            <Stop stopColor="#FFC48D" />
-            <Stop offset="1" stopColor="#FF6A0E" />
-          </LinearGradient>
-        </Defs>
-      </Svg>
-
-
-
-
-      <Svg width="391" height="707" viewBox="0 0 391 707" fill="none">
-        <Path
-          d="M162.735 9.74298L-1 193.5V706.5H393V193.5L204.72 8.35785C192.901 -3.26409 173.762 -2.63268 162.735 9.74298Z"
-          fill="url(#paint0_linear_2006_173)"
-        />
-        <Defs>
-          <LinearGradient
-            id="paint0_linear_2006_173"
-            x1="196"
-            y1="-13"
-            x2="196"
-            y2="706.5"
-            gradientUnits="userSpaceOnUse"
+            <Mountain number={2}/>
+          </MountainView>
+          <MountainView
+            style={{zIndex: 1}}
           >
-            <Stop stopColor="#A24945" />
-            <Stop offset="0.331" stopColor="#FFC58E" />
-          </LinearGradient>
-        </Defs>
-      </Svg>
-      </ScrollView>
-      
-      :
-        <View>
-        <Text>챌린지 화면</Text>
-          <TouchableOpacity
-              onPress={() => {
-                navigation.navigate("Login");
-              }}
-            >
-              <Text style={{fontSize: 30}}>로그인 화면</Text>
-            </TouchableOpacity>
-          <TouchableOpacity
-              onPress={() => {
-                navigation.navigate("MyPage");
-              }}
-            >
-              <Text style={{fontSize: 30}}>마이페이지</Text>
-            </TouchableOpacity>
-          <TouchableOpacity
-              onPress={() => {
-                navigation.navigate("CreateTeam");
-              }}
-            >
-              <Text style={{fontSize: 30}}>팀 생성</Text>
-            </TouchableOpacity>
-          <TouchableOpacity
-              onPress={() => {
-                navigation.navigate("MarathonEntryForm");
-              }}
-            >
-              <Text style={{fontSize: 30}}>마라톤 신청서</Text>
-            </TouchableOpacity>
-          <TouchableOpacity
-              onPress={() => {
-                navigation.navigate("MarathonInfoDetail");
-              }}
-            >
-              <Text style={{fontSize: 30}}>마라톤 정보 디테일</Text>
-            </TouchableOpacity>
-
-        </View>
-      
-      }
-    </Wrapper>
+            <Mountain number={3}/>
+          </MountainView>
+          <MountainView
+            style={{zIndex: 0}}
+          >
+            <Mountain number={4}/>
+          </MountainView>
+          <MountainView
+            style={{zIndex: -1}}
+          >
+            <Mountain number={5}/>
+          </MountainView>
+      </Wrapper>
+    </BackColor>
   );
 };
 export default ChallengeScreen;
--- front/screens/Login/LoginScreen.js
+++ front/screens/Login/LoginScreen.js
@@ -1,121 +1,145 @@
-import React from "react";
-import { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { LinearGradient } from "expo-linear-gradient";
 import MaskedView from "@react-native-masked-view/masked-view";
 import color from "../../styles/colors";
-import backImg from "./../../assets/images/Login_Back_cut2.jpg"
-import { 
+import backImg from "./../../assets/images/Login_Back_cut2.jpg";
+import {
   Container,
   Logo,
   Wrap,
- } from "./LoginScreenStyles";
-import { StyleSheet,View, Text, SafeAreaView, Image } from "react-native";
+} from "./LoginScreenStyles";
+import { View, Dimensions, Image } from "react-native";
 import { useFontsLoaded } from "../../utils/fontContext";
-import { Dimensions } from "react-native";
 import * as Linking from 'expo-linking';
 import * as AuthSession from 'expo-auth-session';
 import useUserStore from "../../store/useUserStore";
 
 import RoundBtn from "../../components/Button/RoundBtn/RoundBtn";
 
-
-// Google OAuth 클라이언트 ID (Google Cloud Console에서 발급받은 ID로 대체하세요)
 const CLIENT_ID = '512721321300-u08i7mqrguoh6oore1gkihr54ukigqh0.apps.googleusercontent.com';
-
-// client-id: 512721321300-u08i7mqrguoh6oore1gkihr54ukigqh0.apps.googleusercontent.com
-// client-secret: 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
-
-
 
 const LoginScreen = ({ navigation }) => {
   const fontsLoaded = useFontsLoaded();
-
-  // 화면 크기에 따라 텍스트 크기 조정
-  const screenWidth = Dimensions.get("window").width;
   const screenHeight = Dimensions.get("window").height;
-
-  // const fontSize = screenWidth * 0.2; // 화면 너비의 20%로 글자 크기 설정
+  const setUser = useUserStore((state) => state.setUser);
 
   if (!fontsLoaded) {
-    return null; // 폰트 로드 전까지 렌더링 방지
+    return null;
   }
-
-  // const checkUser = () => {
-  //   console.log("가입한적 있으면 버튼도 안 보이고 바로 홈 화면으로")
-  //   navigation.navigate("SignUp");
-  // }
-
-  const setUser = useUserStore((state) => state.setUser);
 
   useEffect(() => {
     const handleUrl = ({ url }) => {
       const { queryParams } = Linking.parse(url);
-      const token = queryParams.access_token;
+      const idToken = queryParams.id_token;
   
-      if (token) {
-        console.log('Logged in with Google Token:', token);
-        // 토큰을 상태에 저장하고, 로그인 상태로 전환하거나 다른 화면으로 이동
+      if (idToken) {
+        console.log('Logged in with Google ID Token:', idToken);
+        sendIdTokenToServer(idToken);
       }
     };
   
-    Linking.addEventListener('url', handleUrl);
+    Linking.addEventListener('url', handleUrl); // 기존 addListener 대신 addEventListener 사용
   
     return () => {
-      Linking.removeEventListener('url', handleUrl);
+      Linking.removeEventListener('url', handleUrl); // 기존 remove 대신 removeEventListener 사용
     };
   }, []);
+  
 
+  // 서버로 id_token을 전송하는 함수
+  const sendIdTokenToServer = async (idToken) => {
+    try {
+      const response = await fetch('https://your-server.com/api/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ idToken }),
+      });
+
+      const serverResponse = await response.json();
+      console.log('Server Response:', serverResponse);
+
+      if (serverResponse.success) {
+        // 서버가 인증에 성공하면 사용자 정보 상태 저장
+        setUser({
+          name: serverResponse.name,
+          email: serverResponse.email,
+          imageUrl: serverResponse.picture,
+        });
+        navigation.navigate("Home"); // 인증 후 원하는 화면으로 이동
+      }
+    } catch (error) {
+      console.error("Failed to send ID Token to server:", error);
+    }
+  };
 
   const signInWithGoogle = async () => {
-    // 커스텀 스킴을 사용해 redirectUri 생성
     const redirectUri = AuthSession.makeRedirectUri({
-      scheme: 'maon', // 위에서 설정한 스킴 이름과 동일해야 합니다.
+      scheme: 'maon', // 앱의 커스텀 스킴 설정
     });
-
+  
     // 인증 URL 생성
-    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(
-      redirectUri
-    )}&response_type=token&scope=profile email`;
-
-    // 로그인 요청 보내기
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?` +
+      `response_type=code&` +
+      `client_id=${CLIENT_ID}&` +
+      `scope=openid%20email%20profile&` +
+      `redirect_uri=${encodeURIComponent(redirectUri)}&` +
+      `state=abcd1234&` +
+      `nonce=abcd1234`;
+  
     const result = await AuthSession.startAsync({ authUrl });
-
-    if (result.type === 'success') {
-      // 로그인 성공 시, result.params.access_token에서 액세스 토큰을 가져올 수 있습니다.
-      const token = result.params.access_token;
-      console.log('Google Access Token:', token);
-
-
-      const userInfoResponse = await fetch(
-        'https://www.googleapis.com/userinfo/v2/me',
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
-      const userInfo = await userInfoResponse.json();
-
-      // 상태 저장
-      setUser({
-        name: userInfo.name,
-        email: userInfo.email,
-        imageUrl: userInfo.picture,
-      });
-      // 여기서 토큰을 상태에 저장하고 원하는 화면으로 전환
+  
+    if (result.type === 'success' && result.params.code) {
+      const authorizationCode = result.params.code;
+      console.log('Authorization Code:', authorizationCode);
+  
+      // 서버에 authorizationCode를 보내어 토큰을 교환합니다
+      sendAuthorizationCodeToServer(authorizationCode);
     } else {
       console.log('Google login was canceled or failed.');
+    }
+  };
+
+  const sendAuthorizationCodeToServer = async (authorizationCode) => {
+    try {
+      const response = await fetch('https://your-server.com/api/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ authorizationCode }),
+      });
+  
+      const serverResponse = await response.json();
+      console.log('Server Response:', serverResponse);
+  
+      if (serverResponse.success) {
+        // 서버가 인증에 성공하면 사용자 정보 상태 저장
+        setUser({
+          name: serverResponse.name,
+          email: serverResponse.email,
+          imageUrl: serverResponse.picture,
+        });
+        navigation.navigate("Home"); // 인증 후 원하는 화면으로 이동
+      }
+    } catch (error) {
+      console.error("Failed to send Authorization Code to server:", error);
     }
   };
 
   return (
     <Container>
       <View
-      style={{
-        width: '100%',
-        height: screenHeight,
-        zIndex: 2,
-        position: "absolute",
-        backgroundColor: 'rgba(0, 0, 0, 0.4)',
-      }}
-      ></View>
-      <Image 
+        style={{
+          width: '100%',
+          height: screenHeight,
+          zIndex: 2,
+          position: "absolute",
+          backgroundColor: 'rgba(0, 0, 0, 0.4)',
+        }}
+      />
+      <Image
         source={backImg}
         style={{
           position: "absolute",
@@ -129,11 +153,5 @@ const LoginScreen = ({ navigation }) => {
     </Container>
   );
 };
-
-// const styles = StyleSheet.create({
-//   logo: {
-    
-//   }
-// })
 
 export default LoginScreen;
