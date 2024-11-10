fb645e3eecfd8c1218b27e79d7c0be85ecd2dba8
Kim Seongeun <cellnews0514@gmail.com>
2024-11-10 22:38:29 +0900

    [feat] (김성은) 배포 시에만 되는 로그인 방법 테스트 중

--- front/app.config.js
+++ front/app.config.js
@@ -17,6 +17,9 @@ export default {
         projectId: "7840d08f-0feb-4f5e-9438-7aa843191468"
       }
     },
+    updates: {
+      url: "https://u.expo.dev/7840d08f-0feb-4f5e-9438-7aa843191468"
+    },
     runtimeVersion: "1.0.0"
   },
 };
