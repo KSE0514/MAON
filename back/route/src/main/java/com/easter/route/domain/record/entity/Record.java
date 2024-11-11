1d549c1177b6e58961a8ecf35409020341dbc08a
leedhfsd <leedhfsd@gmail.com>
2024-11-11 14:04:07 +0900

    [feat](이대현) 로그 추가

--- back/route/src/main/java/com/easter/route/domain/record/entity/Record.java
+++ back/route/src/main/java/com/easter/route/domain/record/entity/Record.java
@@ -14,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
@@ -24,6 +25,7 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 @Getter
+@ToString
 public class Record {
 	@Id
 	private String id;
