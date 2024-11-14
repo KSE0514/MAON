3c845ce8688f2a09c5b100c0c982692a6ce3005e
JsooAh <joo1798@naver.com>
2024-11-14 13:56:44 +0900

    [feat] (주수아) 연동 테스트 메시지 송수신, 멤버 정보 전달 부분 구축

--- back/route/src/main/java/com/easter/route/domain/connection/service/ConnectionService.java
+++ back/route/src/main/java/com/easter/route/domain/connection/service/ConnectionService.java
@@ -1,9 +1,13 @@
 package com.easter.route.domain.connection.service;
 
+import com.easter.route.domain.connection.model.dto.ConnectionTestDto;
 import com.easter.route.domain.connection.model.dto.MemberInfoDto;
+import com.easter.route.domain.connection.model.dto.RelayMemberInfoDto;
 
 import java.util.UUID;
 
 public interface ConnectionService {
     void saveMemberInfo(MemberInfoDto dto, String code);
+    ConnectionTestDto connectionTest(String code);
+    RelayMemberInfoDto relayMemberInfo(String code);
 }
--- back/route/src/main/java/com/easter/route/domain/connection/service/ConnectionServiceImpl.java
+++ back/route/src/main/java/com/easter/route/domain/connection/service/ConnectionServiceImpl.java
@@ -1,6 +1,9 @@
 package com.easter.route.domain.connection.service;
 
+import com.easter.route.domain.connection.model.ConnectionType;
+import com.easter.route.domain.connection.model.dto.ConnectionTestDto;
 import com.easter.route.domain.connection.model.dto.MemberInfoDto;
+import com.easter.route.domain.connection.model.dto.RelayMemberInfoDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -8,6 +11,7 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -29,5 +33,26 @@ public class ConnectionServiceImpl implements ConnectionService {
         }
         ops.set(key, dto.getMemberId().toString());
         redisTemplate.expire(key, 10, TimeUnit.MINUTES); // 코드의 유효기간은 10분
+    }
+
+    @Override
+    public ConnectionTestDto connectionTest(String code) {
+        return ConnectionTestDto.builder().type(ConnectionType.CONNECTION_TEST).timestamp(LocalDateTime.now()).build();
+    }
+
+    @Override
+    public RelayMemberInfoDto relayMemberInfo(String code) {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        String key = CODE_PREFIX + code;
+        String memberId = ops.get(key);
+        if(memberId == null) {
+            log.error("cannot find member : {}", code);
+            return null;
+        }
+        return RelayMemberInfoDto.builder()
+                .type(ConnectionType.SENDING_INFORMATION)
+                .memberId(UUID.fromString(memberId))
+                .timestamp(LocalDateTime.now())
+                .build();
     }
 }
