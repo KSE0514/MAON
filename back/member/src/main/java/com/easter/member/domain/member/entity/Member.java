75f70139752a26afa7a9c572c61e2c0814490009
JsooAh <joo1798@naver.com>
2024-11-14 09:54:07 +0900

    [chore] (주수아) 자잘한 수정

--- back/member/src/main/java/com/easter/member/domain/member/entity/Member.java
+++ back/member/src/main/java/com/easter/member/domain/member/entity/Member.java
@@ -4,10 +4,7 @@ import com.easter.member.domain.member.model.Gender;
 import com.fasterxml.uuid.Generators;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.sql.Time;
 import java.time.LocalDate;
@@ -17,6 +14,7 @@ import java.util.UUID;
 
 @Entity
 @Getter
+@ToString
 @Builder(toBuilder = true)
 @Table(name = "member")
 @NoArgsConstructor
