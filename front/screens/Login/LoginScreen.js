65de7794313380e9f19b739229d026e2bfa47825
Kim Seongeun <cellnews0514@gmail.com>
2024-11-16 16:04:11 +0900

    [fix] (김성은) 로그인 시에도 accessToken과 refreshToken을 asyncStorage에 저장하도록 수정

--- front/screens/Login/LoginScreen.js
+++ front/screens/Login/LoginScreen.js
@@ -140,6 +140,7 @@ const LoginScreen = () => {
 
           // 토큰을 AsyncStorage에 저장하여 자동 로그인 활성화
           await AsyncStorage.setItem("accessToken", responseUserInfo.accessToken);
+          await AsyncStorage.setItem("refreshToken", responseUserInfo.refreshToken);
 
           // navigation.navigate("MainTabs", { screen: "Home" });
           navigation.reset({
