683a1d00c918454b98b788fa90ae9dd71d7837c9
dpqls0356 <dpqls0356@gmail.com>
2024-11-16 23:52:21 +0900

    [chore](이예빈)워치연동 여부 alert

--- front/utils/checkPairedWatch.js
+++ front/utils/checkPairedWatch.js
@@ -1,9 +1,12 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Alert } from "react-native";
 
 export const fetchPairedWatch = async () => {
   try {
     const storedPairedWatch = await AsyncStorage.getItem("pairedWatch");
     console.log("Stored Paired Watch:", storedPairedWatch);
+
+    Alert.alert("워치연동 여부: ", storedPairedWatch);
     return storedPairedWatch;
   } catch (error) {
     console.error("Error fetching paired watch:", error);
