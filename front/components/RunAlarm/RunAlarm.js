175871a00f98e154c480e039bf6647570b139173
dpqls0356 <dpqls0356@gmail.com>
2024-11-19 00:11:48 +0900

    [style](이예빈)달리기 시 알림창 디자인 변경

--- front/components/RunAlarm/RunAlarm.js
+++ front/components/RunAlarm/RunAlarm.js
@@ -3,18 +3,29 @@ import { useState } from "react";
 import Svg, { Path } from "react-native-svg";
 import color from "../../styles/colors";
 import { ModalContainer, ModalContent } from "./RunAlarmStyle";
+import fonts from "../../styles/fonts";
 
 const RunAlarm = ({ ment, isVisible }) => {
   if (!isVisible) return null;
   return (
     <ModalContainer style={styles.container}>
-      <ModalContent style={styles.box}>
-        <Text>{ment}</Text>
+      <ModalContent style={styles.textBox}>
+        <Text style={styles.text}>{ment}</Text>
       </ModalContent>
     </ModalContainer>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textBox: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontFamily: fonts.gMarketBold,
+    fontSize: 20,
+    lineHeight: 28, // 줄 높이 추가 (폰트 크기의 1.4배 정도 추천)
+  },
+});
 
 export default RunAlarm;
